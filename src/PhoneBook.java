840f67c552c815123d3b2b1db7c2451cec7a3e09
Дмитрий Ляпунов <dmitry.lapynov@corp.mail.ru>
2020-10-14 01:33:00 +0300

    Read user credentials

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,7 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class PhoneBook {
+
+    public static final String[] TERMINAL_PROMPTER = new String[]{
+        "Введите фамилию",
+        "Введите имя",
+        "Введите отчество",
+        };
 
     public static void main(String[] args) {
         //Добавить считывание ввода пользователя в цикле
+
+        String[] user = new String [3];
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < user.length; i++) {
+            System.out.println(TERMINAL_PROMPTER[i]);
+            String s = scanner.nextLine();
+            user[i] = s;
+        }
+
+        System.out.println(Arrays.toString(user));
     }
 
     public static boolean checkPhoneNumber(String phoneNumber) {
