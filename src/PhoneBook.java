bf55fb60e6ea91dddb0a07a8a65ea15790f59727
Дмитрий Ляпунов <dmitry.lapynov@corp.mail.ru>
2020-10-16 00:00:14 +0300

    Prompt user full name func & check func.

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,26 +1,50 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class PhoneBook {
 
-    public static final String[] TERMINAL_PROMPTER = new String[]{
-        "Введите фамилию",
-        "Введите имя",
-        "Введите отчество",
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+
+    public static final String[] TERMINAL_PROMPTER = new String[] {
+        "Введите фамилию:",
+        "Введите имя:",
+        "Введите отчество:",
         };
+    public static final Scanner SCANNER = new Scanner(System.in);
+
+    public static String[][] phoneBook = new String[5][2];
+    public static int index;
 
     public static void main(String[] args) {
         //Добавить считывание ввода пользователя в цикле
 
-        String[] user = new String [3];
-        Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < user.length; i++) {
+
+        String userFullName;
+        do {
+            /* Узнаем ФИО */
+            userFullName = promptUserFullName();
+        }
+        /* проверяем на формат: 3 слова, эвристику на тип слова делать пока не будем ;) */
+        while(!checkName(userFullName));
+
+        System.out.print(userFullName);
+        
+    }
+
+    private static String promptUserFullName() {
+        String[] userCred = new String[3];
+        for (int i = 0; i < userCred.length; i++) {
             System.out.println(TERMINAL_PROMPTER[i]);
-            String s = scanner.nextLine();
-            user[i] = s;
+            String s = SCANNER.nextLine().trim();
+            userCred[i] = s;
         }
 
-        System.out.println(Arrays.toString(user));
+        return createFullName(userCred);
+    }
+
+    public static String createFullName(String[] cred) {
+        return String.format("%s %s %s", cred[0], cred[1], cred[2]).trim();
     }
 
     public static boolean checkPhoneNumber(String phoneNumber) {
@@ -28,7 +52,13 @@ public class PhoneBook {
     }
 
     public static boolean checkName(String name) {
-        return true;
+        if (name.split("\\s").length == 3) {
+            return true;
+        }
+
+        logError("Неверный формат имени. Ждем от вас инициалы в формате: Фамилия Имя Отчество");
+
+        return false;
     }
 
     public static String formatName(String name) {
@@ -45,5 +75,13 @@ public class PhoneBook {
 
     public static void list(String[][] book) {
         //print phone book
+    }
+
+    private static void logInfo(String info) {
+        System.out.println(ANSI_BLUE + info + ANSI_RESET);
+    }
+
+    private static void logError(String info) {
+        System.out.println(ANSI_RED + info + ANSI_RESET);
     }
 }
