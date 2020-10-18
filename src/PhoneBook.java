164bd700795b6a081bad5e8c923575c08dd595c9
Дмитрий Ляпунов <dmitry.lapynov@corp.mail.ru>
2020-10-18 21:55:44 +0300

    Additional-1. Choosing type of users search/add to book.

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 
 public class PhoneBook {
 
+    /* энумы пока не в счет */
+    public static final String NAME = "NAME";
+    public static final String PHONE = "PHONE";
+
     public static final String STRING_DIVIDER = ",";
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_BLUE = "\u001B[34m";
-
     public static final String[] TERMINAL_PROMPTER = new String[] {
         "Введите фамилию:",
         "Введите имя:",
@@ -27,7 +30,65 @@ public class PhoneBook {
         add(phoneBook, "Алмазов Б В", formatPhoneNumber("01112223344"));
         add(phoneBook, "Януш Д Е", formatPhoneNumber("21112223344"));
 
+        list(phoneBook);
 
+        System.out.println("\nИскать по номеру телефона? (\"+\" – да, \"-\" – нет)");
+        if ("+".equals(SCANNER.nextLine().trim())) {
+            System.out.println("Поиск по телефону\n");
+
+            String phone = formatPhoneNumber(askUserPhoneNumber());
+            logInfo("Телефон: " + phone);
+
+            /* проверяем есть ли такая птица в нашей книге */
+            String user = getUserInfoByType(PHONE, phone, phoneBook);
+
+            if (user == null) {
+                String userFullName = askUserFullName();
+
+                logInfo(userFullName);
+
+                add(phoneBook, userFullName, phone);
+
+            } else {
+                logInfo(String.format("Номер телефона %s найден.\nИмя пользователя: %s", phone, user));
+
+            }
+        } else {
+            System.out.println("Поиск по имени\n");
+
+            String userFullName = askUserFullName();
+            logInfo("Пользователь: " + userFullName);
+
+            /* проверяем есть ли такой телефон в нашей книге */
+            String phone = getUserInfoByType(NAME, userFullName, phoneBook);
+
+            if (phone == null) {
+                phone = formatPhoneNumber(askUserPhoneNumber());
+
+                logInfo(phone);
+
+                add(phoneBook, userFullName, phone);
+
+            } else {
+                logInfo(String.format("Пользователь %s найден.\nНомер телефона: %s", userFullName, phone));
+            }
+        }
+
+        list(phoneBook);
+    }
+    /** Спрашиваем номер телефона */
+    private static String askUserPhoneNumber() {
+        System.out.println("Введите номер телефона:");
+        String phone;/* Читаем номер телефона пока не введется соответствующий нашим высоким стандартам (> 10 && < 11) */
+        do {
+            phone = cleanPhoneNumber(SCANNER.nextLine());
+        }
+        while (!checkPhoneNumber(phone));
+        return phone;
+    }
+
+    /** Спрашиваем ФИО пользователя */
+    private static String askUserFullName() {
         String userFullName;
         do {
             /* Узнаем ФИО */
@@ -36,49 +97,22 @@ public class PhoneBook {
         /* проверяем на формат: 3 слова, эвристику на тип слова делать пока не будем ;) */
         while(!checkName(userFullName));
 
-        logInfo("\n" + userFullName + "\n");
-
-        /* проверяем есть ли такая птица в нашей книге */
-        String phone = getPhoneNumberByFullName(phoneBook, userFullName);
-
-        if (phone == null) {
-            System.out.println("Введите номер телефона:");
-
-            /* Читаем номер телефона пока не введется соответствующий нашим высоким стандартам (> 10 && < 11) */
-            do {
-                phone = cleanPhoneNumber(SCANNER.nextLine());
-            }
-            while (!checkPhoneNumber(phone));
-
-            String phoneFormatted = formatPhoneNumber(phone);
-
-            logInfo(phoneFormatted);
-
-            /* Создаем новый массив, в 2 раза большего размера */
-            if (phoneBook.length == totalRecords) {
-                logInfo("Телефонная книга заканчивается.. приклеиваем новые странички.");
-                phoneBook = Arrays.copyOf(phoneBook, Math.min(totalRecords << 1, Integer.MAX_VALUE));
-            }
-
-            add(phoneBook, userFullName, phoneFormatted);
-
-            list(phoneBook);
-
-        } else {
-            logInfo(String.format("Пользователь найден. Номер телефона: %s", phone));
-        }
+        return userFullName;
     }
 
-    private static String getPhoneNumberByFullName(String[][] phoneBook, String userFullName) {
+    /** Ищем по типу данных. Типов у нас два и в рамах задачи они стринговые: "NAME" и "PHONE" */
+    private static String getUserInfoByType(String dataType, String key, String[][] phoneBook) {
+        boolean isLookupByName = NAME.equalsIgnoreCase(dataType);
         for (String[] userRow : phoneBook) {
-            if (userFullName.equals(userRow[0])) {
-                return userRow[1];
+            if (key.equals(userRow[isLookupByName ? 0 : 1])) {
+                return userRow[isLookupByName ? 1 : 0];
             }
         }
 
         return null;
     }
 
+    /** Спрашиваем и читаем ФИО пользователя */
     private static String promptUserFullName() {
         String[] userCred = new String[3];
         for (int i = 0; i < userCred.length; i++) {
@@ -90,6 +124,7 @@ public class PhoneBook {
         return createFullName(userCred);
     }
 
+    /** Создаем строку с полными инициалами пользователя */
     public static String createFullName(String[] cred) {
         return String.format("%s %s %s", cred[0], cred[1], cred[2]).trim();
     }
@@ -113,6 +148,7 @@ public class PhoneBook {
         return true;
     }
 
+    /** Проверка имени по количеству слов. Так себе проверка, но пока хватит */
     public static boolean checkName(String name) {
         if (name.split("\\s").length == 3) {
             return true;
@@ -123,6 +159,7 @@ public class PhoneBook {
         return false;
     }
 
+    /** Форматируем телефон по шаблону +7 (ххх) ххх-хх-хх */
     public static String formatPhoneNumber(String number) {
         String phoneClean = number.replaceAll("[\\D]", "");
         if (phoneClean.length() == 10) {
@@ -131,25 +168,41 @@ public class PhoneBook {
             phoneClean = "7" + phoneClean.substring(1);
         }
 
-        return "+" +
-               phoneClean.substring(0, 1) + " " +
-               phoneClean.substring(1, 4) + " " +
-               phoneClean.substring(4, 7) + " " +
-               phoneClean.substring(7, 9) + " " +
-               phoneClean.substring(9);
+        return String.format(
+            "+%s (%s) %s-%s-%s",
+            phoneClean.substring(0, 1),
+            phoneClean.substring(1, 4),
+            phoneClean.substring(4, 7),
+            phoneClean.substring(7, 9),
+            phoneClean.substring(9)
+        );
     }
 
     /**
-     * Добавление записи в книгу
+     * Добавление записи в книгу c проверкой массива на достаточный размер
      */
     public static void add(String[][] book, String name, String number) {
+
+        book = resizeArrayWhenFull(book);
+
         if (book[totalRecords] == null) {
             book[totalRecords] = new String[2];
         }
+
         book[totalRecords][0] = name;
         book[totalRecords][1] = number;
 
         totalRecords++;
+    }
+
+    private static String[][] resizeArrayWhenFull(String[][] book) {
+        /* Создаем новый массив, в 2 раза большего размера */
+        if (book.length == totalRecords) {
+            logInfo("Телефонная книга заканчивается.. приклеиваем новые странички.");
+            return PhoneBook.phoneBook = Arrays.copyOf(book, Math.min(totalRecords << 1, Integer.MAX_VALUE));
+        }
+
+        return PhoneBook.phoneBook;
     }
 
     /**
@@ -157,7 +210,7 @@ public class PhoneBook {
      */
     public static void list(String[][] book) {
 
-        /* Собираем строку из имен, так проще получить массив без сложных структур */
+        /* Собираем строку из имен, так будет проще затем получить массив без сложных структур с помощью split() */
         String names = "";
         for (String[] user : book) {
             if (user != null) {
@@ -168,8 +221,9 @@ public class PhoneBook {
 
         Arrays.sort(namesArray);
 
+        System.out.print("\nТелефонная книга:\n===========\n");
         for (String name : namesArray) {
-            System.out.printf("%s: %s\n", name, getPhoneNumberByFullName(book, name));
+            System.out.printf("%s: %s\n", name, getUserInfoByType(NAME, name, book));
         }
 
     }
@@ -181,4 +235,5 @@ public class PhoneBook {
     private static void logError(String info) {
         System.out.println(ANSI_RED + info + ANSI_RESET);
     }
+
 }
